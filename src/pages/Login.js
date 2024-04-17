de0b63be532da526ae66423ee79ec0f1cf9882d2
Dontula <uday.dontula@innovasolutions.com>
2024-04-17 12:38:38 +0530

    Input Component in Login Form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Input from '../components/core/Input/Input';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Basic validation criteria
+    if (email.trim() === '' || password.trim() === '') {
+      alert('Please enter both email and password.');
+      return;
+    }
+    // Handle login logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
   return (
-    <div>
-      Login Page
-    </div>
+    <form onSubmit={handleSubmit}>
+      <Input
+        type="email"
+        placeholder="Email"
+        // value="email"
+        onChange={handleEmailChange}
+      />
+      <Input
+        type="password"
+        placeholder="Password"
+        // value="password"
+        onChange={handlePasswordChange}
+      />
+      <button type="submit">Login</button>
+    </form>
   );
 }
 
