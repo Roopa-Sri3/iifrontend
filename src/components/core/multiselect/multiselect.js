6e440e4d5d07b457b435e4f17de68baa6b4ebb76
Valishetti <pooja.valishetti@innovasolutions.com>
2024-04-24 12:37:23 +0530

    created Multiselect component

--- src/components/core/multiselect/multiselect.js
+++ src/components/core/multiselect/multiselect.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import OptionItem from './option-item';
+import './multiselect.css';
+import ChevronRight from '../../assets/svgs/ChevronRight';
+import DeSelect from '../../assets/svgs/DeSelect';
 
 const MultiSelect = ({
   id,
@@ -7,13 +10,35 @@ const MultiSelect = ({
   options,
   checked,
   onChange,
-})=>{
+  getValues,
+  setValues,
+  selectedValues,
+}) => {
   const [isMenuOpen,setIsMenuOpen] = useState(false);
-  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState(selectedValues ?? []);
   const [searchItem,setSearchItem] = useState('');
+  const dropboxRef = useRef(null);
+
+  useEffect(() => {
+    setSelectedOptions(selectedValues ?? []);
+  }, [selectedValues]);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleMouseClick = () => {
+    toggleMenu();
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' || event.key === " ") {
+      toggleMenu();
+    }
+  };
+
+  const handleTouch = () => {
+    toggleMenu();
   };
 
   const handleSearchChange = (event) =>{
@@ -24,54 +49,90 @@ const MultiSelect = ({
     handleOptionClick(option);
   };
 
-  const handleOptionClick = (option) = {
-    if (selectedOptions.includes(option)) {
-      setSelectedOptions(selectedOptions.filter((item) => item !== option));
-    } else {
-      setSelectedOptions([...selectedOptions,option]);
+  const handleOutsideClick = (event) => {
+    if(dropboxRef.current && !dropboxRef.current.contains(event.target)) {
+      setIsMenuOpen(false);
+      setSearchItem('');
     }
   };
 
-  const filterOptions = options.filter(option => option.includes(searchItem));
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
+  const handleOptionClick = (option) => {
+    const updatedSelectedOptions = Array.isArray(selectedOptions) &&
+    selectedOptions.includes(option)
+      ? selectedOptions.filter((item) => item !== option)
+      : [...selectedOptions,option];
+    setSelectedOptions(updatedSelectedOptions);
+    setSearchItem('');
+    onChange(updatedSelectedOptions);
+  };
+
+  const filterOptions = options.filter(option =>
+    option.label.toLowerCase().includes(searchItem.toLowerCase()));
 
   return (
-    <div className="drop-box">
-      <div className="drop-box-header" onClick={toggleMenu}>
-        {label} <span className="drop-box-icon">{isMenuOpen ? '<' : '>'}</span>
+    <div className="drop-box" ref={dropboxRef}>
+      <div className={`drop-box-header 
+      ${isMenuOpen ? 'grey-border' : 'red-color'}`}
+      role="button"
+      tabIndex="0"
+      onClick={handleMouseClick}
+      onKeyDown={handleKeyDown}
+      onTouch={handleTouch}
+      >
+        <div className="selected-options">
+          {selectedOptions && selectedOptions.map(option => (
+            <div key={option.id} className="selected-option">
+              {option.label}
+              <span className="deselect-option"
+                role="button"
+                tabIndex="0"
+                onClick = {() => handleOptionClick(option)}
+                onKeyDown={(event) => {
+                  if(event.key === "Enter" || event.key === " "){
+                    handleOptionClick(option);
+                  }
+                }}
+              >
+                {selectedOptions.includes(option) && (<DeSelect/>)}
+              </span>
+            </div>
+          ))}
+        </div>
+        {label} <span className="drop-box-icon">
+          {<ChevronRight />}
+        </span>
       </div>
       {isMenuOpen && (
         <div className='drop-box-menu'>
           <input
             type="text"
-            value="searchItem"
+            placeholder="Search"
+            value={searchItem}
             onChange={handleSearchChange}
           />
-          {filterOptions.map(option=>(
-            <OptionItem
-            key={option.id}
-              id={option.id}
-              label={option.label}
-              value={option.value}
-              checked={selectedOptions.includes(option)}
-              onChange={()=>{handleCheckbox(option)}}
-            />
-          ))}
+          <div className="options-menu">
+            {filterOptions.map(option=>(
+              <OptionItem
+                key={option.id}
+                id={option.id}
+                label={option.label}
+                value={option.value}
+                checked={selectedOptions.includes(option)}
+                onChange={() => {handleCheckbox(option);
+                }}
+              />
+            ))}
+          </div>
         </div>
       )}
-      <div>
-        {selectedOptions.map(option => (
-          <div key={option.id} className="selected-option">
-            {option.label}
-            <span
-              className="deselect-option"
-              onClick = {() => handleOptionClick(option)}
-            >
-              *
-            </span>
-          </div>
-        ))}
-      </div>
-    </div>    
+    </div>
   );
 };
 
--- src/components/core/multiselect/option-item.js
+++ src/components/core/multiselect/option-item.js
@@ -8,14 +8,15 @@ const OptionItem = ({
   onChange,
 }) => {
   return (
-    <div>
+    <div className="option-item">
       <input
+        className="check-box"
         type="checkbox"
         id={id}
         value={value}
         checked={checked}
         onChange={onChange}
-      />
+      />{" "}
       <label htmlFor={id}>{label}</label>
     </div>
   );
