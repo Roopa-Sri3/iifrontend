9f092c7f8ebff85711e39f8e8ee740315ab16069
Dontula <uday.dontula@innovasolutions.com>
2024-04-25 19:50:32 +0530

    Login functionality completed

--- src/store/selector/app/app.js
+++ src/store/selector/app/app.js
@@ -3,6 +3,3 @@ export const IsUserLoggedIn = (state) => state.app.isUseLoggedIn;
 export const GetUserName = (state) => state.app.userName;
 
 export const IsModalOpen = (state, name) => state.app.modal.modalName === name;
-
-export const GetProfileShortcut = (state) => {};
-
