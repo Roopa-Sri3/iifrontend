8e70d6c46d2d1a54ed75312c45735606540b0bdc
saisk123 <19641a05j1@gmail.com>
2024-04-25 11:39:14 +0530

    Created Generic Alert and Toaster components

--- src/store/selector/app/app.js
+++ src/store/selector/app/app.js
@@ -4,3 +4,8 @@ export const GetUserName = (state) => state.app.userName;
 
 export const IsModalOpen = (state, name) => state.app.modal.modalName === name;
 
+export const GetAlertMessage = (state) => state.app.message;
+
+export const GetAlertMessageType = (state) => state.app.messageType;
+
+export const GetTimeoutId = (state) => state.app.timeoutId;
