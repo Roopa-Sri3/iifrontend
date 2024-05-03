bddd4b35112d6823534a228061993e6e54613345
saisk123 <19641a05j1@gmail.com>
2024-05-03 09:34:06 +0530

    Added Textarea

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,41 +1,71 @@
+import { useState } from 'react';
 import Button from '../core/button';
 import './Question.css';
 
 const Question = ({
+  questionNumber,
+  totalQuestions,
   questionText,
-  options
+  category,
+  handlePrevious,
+  handleSave,
+
 }) => {
+  const [codeValue, setCodeValue] = useState('');
 
-  const handlePrevious = () =>{
-
+  const handlePreviousButton = () =>{
+    handlePrevious();
   };
 
-  const handleSave = () =>{
+  const handleSaveButton = () =>{
+    handleSave();
+  };
 
+  const handleCodeChange = (event) => {
+    setCodeValue(event.target.value);
   };
 
   return (
     <div className='questions-container'>
       <div className='question-card'>
-        <h4 className='number'>Question 8/15</h4>
+        <h4 className='number'>Question {questionNumber}/{totalQuestions}</h4>
         <div className='question'>
           <h6 className='heading'>Answer the Question</h6>
           <div className='question-text'>
             {questionText}
           </div>
-          <div className='options'>
-            {options}
-          </div>
+          {category === 'MULTISELECT' &&
+            <div className='multi-option'>
+
+            </div>
+          }
+          {category === 'SINGLESELECT' &&
+            <div className='sinlge-option'>
+
+            </div>
+          }
+          {category === 'CODING' &&
+            <div className='coding'>
+              Write code here
+              <textarea
+                className='coding-area'
+                value={codeValue}
+                onChange={handleCodeChange}
+                rows={10}
+                cols={175}
+              />
+            </div>
+          }
         </div>
         <div className='action-buttons'>
           <Button
             className={'previous-button'}
             label={'Previous'}
-            handleClick={handlePrevious} />
+            handleClick={handlePreviousButton} />
           <Button
             className={'save-next'}
             label={'Save & Next'}
-            handleClick={handleSave}/>
+            handleClick={handleSaveButton}/>
         </div>
       </div>
     </div>
