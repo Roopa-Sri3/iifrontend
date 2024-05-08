2bd89aa47fdeb6469ab6b7ff507520ed4c55794b
Ganga <tharunkumar.ganga@innovasolutions.com>
2024-05-08 10:36:01 +0530

    added infocard component and CandidateProfileview.js

--- src/components/core/CandidateProfileView/CandidateProfileView.js
+++ src/components/core/CandidateProfileView/CandidateProfileView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CallIcon from '../../assets/svgs/CallIcon';
 import MailIcon from '../../assets/svgs/MailIcon';
 import ExperienceIcon from '../../assets/svgs/ExperienceIcon';
+import Infocard from './Infocard/Infocard';
 import './CandidateProfileView.css';
 
 function CandidateProfileView() {
@@ -9,14 +10,23 @@ function CandidateProfileView() {
     <div className='Candidate-box'>
       <div className="vertical-line"></div>
       <div className="icon-wrapper">
-        <CallIcon />
+        <div className='icon-container'>
+          <div className="icon-item">
+            <CallIcon />
+          </div>
+          <div className="icon-item">
+            <MailIcon />
+          </div>
+          <div className="icon-item">
+            <ExperienceIcon />
+          </div>
+        </div>
       </div>
-      <div className="icon-wrapper">
-        <MailIcon />
+      <div className = "support-email">
+        <p>For further queries reach out to
+         firstname.lastname@innovasolutions.com </p>
       </div>
-      <div className="icon-wrapper">
-        <ExperienceIcon />
-      </div>
+      <Infocard/>
     </div>
   );
 }
---
+++ src/components/core/CandidateProfileView/Infocard/Infocard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function  Infocard(){
+  return (
+    <div className = "box">
+    </div>
+  );
+}
+
+export default Infocard;
