8d7ef4427d3c13beae67db5de6dae41fde2054a0
Dontula <uday.dontula@innovasolutions.com>
2024-05-23 12:37:09 +0530

    Completed session storage for application

--- src/privateRoutes/protectDashboardRoute/ProtectedDashboardRoute.js
+++ src/privateRoutes/protectDashboardRoute/ProtectedDashboardRoute.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { IsUserLoading, IsUserLoggedIn } from "../../store/selector/app";
 
 const ProtectedDashboardRoute = () => {
-  const navigate = useNavigate();
   const isUserLoading = useSelector(IsUserLoading);
   const isUserLoggedIn = useSelector(IsUserLoggedIn);
-
-  if(!isUserLoggedIn){
-    navigate("/unauthorized");
-  }
 
   if (isUserLoading) {
     return <>Loading...</>;
