d5289e4a925b917bb71daa1bceb423272cc50990
Boosani <sidharth.boosani@innovasolutions.com>
2024-05-29 10:20:50 +0530

    Removed the Commented Code

--- src/store/reducers/screen/screen.js
+++ src/store/reducers/screen/screen.js
@@ -7,59 +7,6 @@ const initialState = {
   isRunning: null,
   currentQuestion : 1,
   assessmentId: null,
-  // questions: [
-  //   {
-  //     questionId: 1,
-  //     questionText: "What is the capital of France?",
-  //     options: [
-  //       { label:"Paris", value:"paris"},
-  //       { label:"London", value:"london"},
-  //       { label:"Berlin", value:"berlin"},
-  //       { label:"Madrid", value:"madrid"},],
-  //     questionType: "single-select"
-  //   },
-  //   {
-  //     questionId: 2,
-  //     questionText: "What is the largest planet in our solar system?",
-  //     options: [{ label:"Earth", value:"earth"},
-  //       { label:"Mercury", value:"mercury"},
-  //       { label:"Saturn", value:"saturn"},
-  //       { label:"Jupiter", value:"jupiter"},],
-  //     questionType: "single-select",
-  //   },
-  //   {
-  //     questionId: 3,
-  //     questionText: "Who is the highest scorer of IPL 2016?",
-  //     options: [{ label:"Virat", value:"virat"},
-  //       { label:"Gill", value:"gill"},
-  //       { label:"Sachin", value:"sachin"},
-  //       { label:"Dhoni", value:"dhoni"},],
-  //     questionType: "single-select",
-  //   },
-  //   {
-  //     questionId: 4,
-  //     questionText: "Write a C program to print first 10 multiples of 2?",
-  //     questionType: "coding"
-  //   },
-  //   {
-  //     questionId: 5,
-  //     questionText: "Who is the highest scorer of IPL 2016?",
-  //     options: [{ label:"Virat", value:"virat"},
-  //       { label:"Gill", value:"gill"},
-  //       { label:"Sachin", value:"sachin"},
-  //       { label:"Dhoni", value:"dhoni"},],
-  //     questionType: "single-select",
-  //   },
-  //   {
-  //     questionId: 6,
-  //     questionText: "Who is the highest scorer of IPL 2016?",
-  //     options: [{ label:"Virat", value:"virat"},
-  //       { label:"Gill", value:"gill"},
-  //       { label:"Sachin", value:"sachin"},
-  //       { label:"Dhoni", value:"dhoni"},],
-  //     questionType: "single-select",
-  //   },
-  // ],
   questions :[],
   answers: [],
 };
