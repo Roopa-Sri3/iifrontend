814e73ff72f7854c9d859e5dcbdc20b3f692325b
Ganga <tharunkumar.ganga@innovasolutions.com>
2024-05-31 13:39:43 +0530

    added selectors

--- src/components/core/CandidateProfileView/CandidateProfileView.js
+++ src/components/core/CandidateProfileView/CandidateProfileView.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
 import { setAlert } from "../../../store/reducers/app/app";
 import { useNavigate } from "react-router-dom";
 import { PostIdProofDetails } from "../../../../src/store/reducers/candidate/candidate";
 import DocumentUploader from "../documentUploader/documentUploader";
 import RightArrowIcon from "../../../assets/svgs/rightArrowIcon";
-import CallIcon from "../../assets/svgs/CallIcon";
-import MailIcon from "../../assets/svgs/MailIcon";
-import ExperienceIcon from "../../assets/svgs/ExperienceIcon";
+import CallIcon from "../../../assets/svgs/CallIcon";
+import MailIcon from "../../../assets/svgs/MailIcon";
+import ExperienceIcon from "../../../assets/svgs/ExperienceIcon";
 import Infocard from "./Infocard/Infocard";
 import "./CandidateProfileView.css";
-import { candidateDetails } from "../../../components/core/CandidateProfileView/Infocard/Constants";
+import {
+  GetCandidateName,
+  GetPhoneNumber,
+  GetEmail,
+  GetExperience,
+  GetPrimarySkill,
+  GetSecondarySkills,
+  GetRrNo,
+} from "../../../store/selector/app/app";
 import VerticalLine from "../../../assets/svgs/VerticalLine";
 import "./CandidateProfileView.css";
 
@@ -21,6 +29,13 @@ function CandidateProfileView() {
   const [errorMessage, setErrorMessage] = useState("");
   const [isValidFile, setIsValidFile] = useState(false);
   const [uploadFailed, setUploadFailed] = useState(false);
+  const candidateName = useSelector(GetCandidateName);
+  const phoneNumber = useSelector(GetPhoneNumber);
+  const email = useSelector(GetEmail);
+  const experience = useSelector(GetExperience);
+  const primarySkill = useSelector(GetPrimarySkill);
+  const secondarySkills = useSelector(GetSecondarySkills);
+  const rrNo = useSelector(GetRrNo);
 
   const handleNextPage = () => {
     navigate("/candidate/assessment-instructions");
@@ -76,25 +91,25 @@ function CandidateProfileView() {
               <div className="icon-wrapper">
                 <div className="icon-container">
                   <div className="candidate-name">
-                    {candidateDetails.candidateName}
-                    <Infocard text={candidateDetails.uniqueNumber} background="green-background" />
+                    {candidateName}
+                    <Infocard text={rrNo} background="green-background" />
                   </div>
                   <div className="icon-item">
                     <CallIcon />
                     <span className="candidate-number">
-                      {candidateDetails.phoneNumber}
+                      {phoneNumber}
                     </span>
                   </div>
                   <div className="icon-item">
                     <MailIcon />
                     <span className="candidate-mail">
-                      {candidateDetails.Email}
+                      {email}
                     </span>
                   </div>
                   <div className="icon-item">
                     <ExperienceIcon />
                     <span className="candidate-experience">
-                      {candidateDetails.Experience}
+                      {experience}
                     </span>
                   </div>
                 </div>
@@ -108,17 +123,17 @@ function CandidateProfileView() {
                 <p>Skills:</p>
                 <div className="skills">
                   <div className="primary-skills">
-                    <Infocard text={candidateDetails.primarySkills} background="blue-background" />
+                    <Infocard text={primarySkill} background="blue-background" />
                   </div>
                   <div className="secondary-skills">
-                    <Infocard text={candidateDetails.secondarySkills} background="blue-background" />
+                    <Infocard text={secondarySkills} background="blue-background" />
                   </div>
                 </div>
               </div>
             </div>
           </div>
           <div className="email-address-container">
-            {candidateDetails.supportEmail}
+            {/* {supportEmail} */}
           </div>
         </div>
       </div>
--- src/store/selector/candidate/candidate.js
+++ src/store/selector/candidate/candidate.js
@@ -1,1 +1,13 @@
 export const GetCandidateName = (state) => state.candidate.candidateName;
+
+export const GetPhoneNumber = (state) => state.candidate.phoneNumber;
+
+export const GetEmail = (state) => state.candidate.email;
+
+export const GetExperience = (state) => state.candidate.experience;
+
+export const GetPrimarySkill = (state) => state.candidate.primarySkill;
+
+export const GetSecondarySkills = (state) => state.candidate.secondarySkills;
+
+export const GetRrNo = (state) => state.candidate.rrNo;
