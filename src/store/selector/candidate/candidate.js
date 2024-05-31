b7cae1d83c7a7a03594b18fab6f15099f3527aa6
Ganga <tharunkumar.ganga@innovasolutions.com>
2024-05-31 15:49:17 +0530

    removed unnecessary data

--- src/store/selector/candidate/candidate.js
+++ src/store/selector/candidate/candidate.js
@@ -2,14 +2,3 @@ export const GetCandidateName = (state) => state.candidate.candidateName;
 
 export const GetCandidateId = (state) => state.candidate.candidateId;
 
-export const GetPhoneNumber = (state) => state.candidate.phoneNumber;
-
-export const GetEmail = (state) => state.candidate.email;
-
-export const GetExperience = (state) => state.candidate.experience;
-
-export const GetPrimarySkill = (state) => state.candidate.primarySkill;
-
-export const GetSecondarySkills = (state) => state.candidate.secondarySkills;
-
-export const GetRrNo = (state) => state.candidate.rrNo;
