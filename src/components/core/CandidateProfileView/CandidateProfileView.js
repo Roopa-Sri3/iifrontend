50eed230619084e69a4934c6df17d7a85ec8d71c
Rajeev <aparna.rajeev@innovasolutions.com>
2024-06-03 15:12:05 +0530

    Commit with latest changes in the upload part

--- src/components/core/CandidateProfileView/CandidateProfileView.js
+++ src/components/core/CandidateProfileView/CandidateProfileView.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setAlert } from "../../../store/reducers/app/app";
 import { useNavigate } from "react-router-dom";
 import { PostIdProofDetails } from "../../../../src/store/reducers/candidate/candidate";
+import { GetCandidateEmail, GetCandidateExperience, GetCandidateName, GetCandidateNumber, GetCandidatePrimarySkill, GetCandidateRrno, GetCandidateSecondarySkills, GetStoreSkillsOptions } from "../../../store/selector/candidate/candidate";
 import DocumentUploader from "../documentUploader/documentUploader";
 import RightArrowIcon from "../../../assets/svgs/rightArrowIcon";
 import CallIcon from "../../../../src/assets/svgs/CallIcon";
@@ -11,8 +12,6 @@ import ExperienceIcon from "../../../../src/assets/svgs/ExperienceIcon";
 import Infocard from "./Infocard/Infocard";
 import VerticalLine from "../../../assets/svgs/VerticalLine";
 import "./CandidateProfileView.css";
-import { GetCandidateEmail, GetCandidateExperience, GetCandidateName, GetCandidateNumber, GetCandidatePrimarySkill, GetCandidateRrno, GetCandidateSecondarySkills, GetStoreSkillsOptions } from "../../../store/selector/candidate/candidate";
-
 function CandidateProfileView() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -158,7 +157,7 @@ function CandidateProfileView() {
             <p className="id-proof-text">Upload files for ID proof</p>
             <DocumentUploader
               displayText="Click to Upload PAN/Aadhar"
-              secondaryText="Supported files JPEG, JPG, PNG with max size 2MB."
+              secondaryText="Supported file formats JPEG,JPG,PNG with max size 2MB."
               handleFiles={(e) => handleFileChange(e.target.files[0])}
               selectedFile={selectedFile}
               errorMessage={errorMessage}
