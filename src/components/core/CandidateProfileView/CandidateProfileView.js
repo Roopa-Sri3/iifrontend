eb99e1f10a2773c529bcc0b82f9a751ed49fbe7f
Rajeev <aparna.rajeev@innovasolutions.com>
2024-06-06 19:01:10 +0530

    Commit with recent figma changes

--- src/components/core/CandidateProfileView/CandidateProfileView.js
+++ src/components/core/CandidateProfileView/CandidateProfileView.js
@@ -1,4 +1,4 @@
-import React, { useRef,useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setAlert } from "../../../store/reducers/app/app";
 import { useNavigate } from "react-router-dom";
@@ -8,7 +8,7 @@ import {
   GetCandidateExperienceInMonths,
   GetCandidateExperienceInYears,
   GetCandidateHR,
-  GetCandidateName ,
+  GetCandidateName,
   GetCandidateNumber,
   GetCandidatePrimarySkill,
   GetCandidateSecondarySkills,
@@ -41,7 +41,13 @@ function CandidateProfileView() {
   const [errorMessage, setErrorMessage] = useState("");
   const [isValidFile, setIsValidFile] = useState(false);
   const [uploadFailed, setUploadFailed] = useState(false);
+  const [isNextButtonEnabled, setIsNextButtonEnabled] = useState(false);
+  const [isFileUploaded, setIsFileUploaded] = useState(false);
   const fileRef = useRef();
+
+  useEffect(() => {
+    setIsNextButtonEnabled(isValidFile && !uploadFailed && isFileUploaded);
+  }, [isValidFile, uploadFailed, isFileUploaded]);
 
   const handleNextPage = () => {
     navigate("/candidate/assessment-instructions");
@@ -53,7 +59,7 @@ function CandidateProfileView() {
       setIsValidFile(
         file.size <= 2 * 1024 * 1024 &&
         file.size >= 10 * 1024 &&
-          ["image/jpeg", "image/jpg", "image/png"].includes(file.type)
+        ["image/jpeg", "image/jpg", "image/png"].includes(file.type)
       );
       setUploadFailed(false);
     }
@@ -64,6 +70,7 @@ function CandidateProfileView() {
     setErrorMessage("");
     setIsValidFile(false);
     setUploadFailed(false);
+    setIsFileUploaded(false);
     fileRef.current.value = null;
   };
 
@@ -77,6 +84,7 @@ function CandidateProfileView() {
             dispatch(setAlert({ message: "File uploaded successfully", messageType: "success" }));
             setErrorMessage("");
             setUploadFailed(false);
+            setIsFileUploaded(true);
           },
           onError: () => {
             setUploadFailed(true);
@@ -84,7 +92,7 @@ function CandidateProfileView() {
           }
         }));
       } else {
-        setErrorMessage("File size or format is incorrect.");
+        setErrorMessage("File format or size is incorrect.");
         setUploadFailed(true);
       }
     }
@@ -92,130 +100,131 @@ function CandidateProfileView() {
 
   const getSkillLabel = (skill) => {
     const filteredSkill = skillsOptions.find((option) => option.value === skill);
-
     return filteredSkill ? filteredSkill.label : "";
   };
 
   return (
     <>
-      <div className="header-container">
-        <div className="candidate-container">
-          <div className="Candidate-box">
-            <div className="Candidate-info-container">
-              <div className="icon-wrapper">
-                <div className="icon-container">
-                  <div className="candidate-title">
-                    <h5>Candidate Details</h5>
-                  </div>
-                  <div className="icon-item">
-                    <AccountCircle />
-                    <span className="candidate-name">
-                      {candidateName}
-                    </span>
-                  </div>
-                  <div className="icon-item">
-                    <CallIcon />
-                    <span className="candidate-number">
-                      {phoneNumber}
-                    </span>
-                  </div>
-                  <div className="icon-item">
-                    <MailIcon />
-                    <span className="candidate-mail">
-                      {email}
-                    </span>
-                  </div>
-                  <div className="icon-item">
-                    <ExperienceIcon />
-                    <span className="candidate-experience">
+      <div className="Candidate-profile-page-container">
+        <div className="header-container">
+          <div className="candidate-container">
+            <div className="Candidate-box">
+              <div className="Candidate-info-container">
+                <div className="icon-wrapper">
+                  <div className="icon-container">
+                    <div className="candidate-title">
+                      <p>Candidate Details</p>
+                    </div>
+                    <div className="icon-item">
+                      <AccountCircle />
+                      <span className="candidate-name">
+                        {candidateName}
+                      </span>
+                    </div>
+                    <div className="icon-item">
+                      <CallIcon />
+                      <span className="candidate-number">
+                        {phoneNumber}
+                      </span>
+                    </div>
+                    <div className="icon-item">
+                      <MailIcon />
+                      <span className="candidate-mail">
+                        {email}
+                      </span>
+                    </div>
+                    <div className="icon-item">
+                      <ExperienceIcon />
+                      <span className="candidate-experience">
                       Experience {years} {years > 1 ? "Yrs" : "Yr"}
-                      <>
-                        {months > 0 && (
-                          <>
-                            {" "}
-                            {months} {months > 1 ? "Months" : "Month"}
-                          </>
-                        )}
-                      </>
-                    </span>
+                        <>
+                          {months > 0 && (
+                            <>
+                              {" "}
+                              {months} {months > 1 ? "Months" : "Month"}
+                            </>
+                          )}
+                        </>
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="Skill-set-container">
-              <div className="vertical-line">
-                <VerticalLine />
-              </div>
-              <div className="skills-container">
-                <p className="skills-text">Skills:</p>
-                <div className="skills">
-                  <div className="primary-skills">
-                    <Infocard
-                      text={getSkillLabel(primarySkill)}
-                      background="blue-background"
-                    />
-                  </div>
-                  <div className="secondary-skills">
-                    {secondarySkills
-                    && secondarySkills.length > 0
-                    && secondarySkills.map((secondarySkill) =>(
+              <div className="Skill-set-container">
+                <div className="vertical-line">
+                  <VerticalLine />
+                </div>
+                <div className="skills-container">
+                  <p>Skills:</p>
+                  <div className="skills">
+                    <div className="primary-skills">
                       <Infocard
-                        text={getSkillLabel(secondarySkill)}
+                        text={getSkillLabel(primarySkill)}
                         background="blue-background"
                       />
-                    ))}
+                    </div>
+                    <div className="secondary-skills">
+                      {secondarySkills
+                      && secondarySkills.length > 0
+                      && secondarySkills.map((secondarySkill) => (
+                        <Infocard
+                          text={getSkillLabel(secondarySkill)}
+                          background="blue-background"
+                        />
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="email-address-container">
-            <p>For further queries reach out to  {candidateHR} </p>
-          </div>
-        </div>
-      </div>
-      <div className="profile-viewer container">
-        <div className="id-proof-title">
-          <p>ID Proof</p>
-        </div>
-        <div className="id-proof-super-container">
-          <div className="id-proof container">
-            <p className="id-proof-text">Upload a file for ID proof</p>
-            <DocumentUploader
-              displayText="Click to Upload PAN/Aadhar"
-              secondaryText="Supported file formats JPEG,JPG,PNG with size between 10KB and 2MB."
-              handleFiles={(e) => handleFileChange(e.target.files[0])}
-              selectedFile={selectedFile}
-              errorMessage={errorMessage}
-              handleDelete={handleDelete}
-              uploadFailed={uploadFailed}
-              fileRef={fileRef}
-            />
-            {errorMessage && <p className="error-document-message">{errorMessage}</p>}
-            <div className="button-layout">
-              <button
-                className="upload-button"
-                onClick={handleSubmit}
-                disabled={!selectedFile || errorMessage}
-              >
-                Upload
-              </button>
+            <div className="email-address-container">
+              <p>For further queries reach out to  {candidateHR} </p>
             </div>
           </div>
         </div>
-        <div className="next-page-container">
-          <div className="button-container">
-            <button
-              className="next-page-button"
-              onClick={handleNextPage}
-              disabled={!isValidFile || uploadFailed}
-            >
-              Next page
-              <div className="arrow-container">
-                <RightArrowIcon />
+        <div className="profile-viewer container">
+          <div className="id-proof-title">
+            <p>ID Proof</p>
+          </div>
+          <div className="id-proof-super-container">
+            <div className="id-proof container">
+              <p className="id-proof-text">Upload file for ID proof</p>
+              <DocumentUploader
+                displayText="Click to Upload PAN/Aadhar"
+                secondaryText="Supported file formats JPEG,JPG,PNG with size between 10KB and 2MB."
+                handleFiles={(e) => handleFileChange(e.target.files[0])}
+                selectedFile={selectedFile}
+                errorMessage={errorMessage}
+                handleDelete={handleDelete}
+                uploadFailed={uploadFailed}
+                fileRef={fileRef}
+              />
+              {errorMessage && <p className="error-document-message">{errorMessage}</p>}
+              <div className="button-layout">
+                <button
+                  className="upload-button"
+                  onClick={handleSubmit}
+                  disabled={!selectedFile || errorMessage}
+                >
+                Upload
+                </button>
               </div>
-            </button>
+            </div>
+          </div>
+          <div className="next-page-container">
+            <div className="button-container">
+              <button
+                className={`next-page-button ${isNextButtonEnabled ? "enabled" : ""}`}
+                onClick={handleNextPage}
+                disabled={!isNextButtonEnabled}
+              >
+              Next page
+                <div className="arrow-container">
+                  <RightArrowIcon />
+                </div>
+              </button>
+            </div>
           </div>
         </div>
       </div>
