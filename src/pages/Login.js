9428789f7e2a47eb16608c95f46e6ab56a38b2d5
Gaddam, Roopa <roopa.gaddam@innovasolutions.com>
2024-06-07 11:19:59 +0000

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -121,7 +121,7 @@ function Login() {
           <div className="label-and-input">
             <Label
               htmlFor="email"
-              text="User Id"
+              text="Email address"
               className="label-text"
             />
             <Input
